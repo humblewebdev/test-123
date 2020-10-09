b0aef683090b0de4c1ae7e27c8779a7f5dcbaabc
Thomas Veilleux <thomas.veilleux.jr@gmail.com>
2020-10-08 21:30:20 -0500

    I am Batman

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
+  rules: {
+    'import/extensions': 'never',
+    'import/first': 0,
+    'import/no-cycle': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': [2, { props: false }],
+    'no-case-declarations': 0,
+    indent: ['error', 2],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathPrefix: '@server',
+        rootPathSuffix: 'server',
+      },
+    },
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
